73e20eb66d89b711d29f8dfa587286af0821f3e5
Mark Stacey <markjstacey@gmail.com>
2020-03-26 12:32:47 -0300

    Create token unit tests (#29)
    The `createSimpleTokenEnvironment` function was extracted from the
    `simple-token` tests into a helper library, and reused for the token
    unit tests.
    
    The token unit tests focused on the constructor specifically, using the
    `serialize` function to assert expectations about the token state. The
    rest of the module has not yet been tested, and neither have conditions
    that are known to make the token fail upon construction. The failure
    conditions will be tested in a later PR; at the moment it's difficult
    to test them because they throw an exception asynchronously, after the
    constructor returns.

---
+++ test/unit/token.js
@@ -0,0 +1,223 @@
+const test = require('tape')
+const BN = require('ethjs').BN
+
+const Token = require('../../lib/token')
+const { setupSimpleTokenEnvironment } = require('../helper')
+
+test('token with minimal options', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    contract,
+    owner: addresses[0],
+  })
+
+  t.deepEqual(
+    token.serialize(),
+    {
+      address: '0x0',
+      symbol: undefined,
+      balance: '0',
+      decimals: 0,
+      string: '0'
+    },
+    'should serialize minimal token correctly',
+  )
+  t.end()
+})
+
+test('token with address', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract, tokenAddress } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    address: tokenAddress,
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.address, tokenAddress, 'should serialize token address correctly')
+  t.end()
+})
+
+test('token with symbol', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    contract,
+    owner: addresses[0],
+    symbol: 'TEST',
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.symbol, 'TEST', 'should serialize token symbol correctly')
+  t.end()
+})
+
+test('token with number balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: 10,
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '10', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with unprefixed hex string balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: '10',
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '16', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with BN.js balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: new BN(10),
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '10', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with array balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: [10],
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '10', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with zero balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: 0,
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '0', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with empty string balance', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    balance: '',
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.balance, '0', 'should serialize token balance correctly')
+  t.end()
+})
+
+test('token with decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: 10,
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 10, 'should serialize token decimals correctly')
+  t.end()
+})
+
+test('token with string decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: '10',
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 10, 'should serialize token decimals correctly')
+  t.end()
+})
+
+test('token with zero decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: 0,
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 0, 'should serialize token decimals correctly')
+  t.end()
+})
+
+test('token with empty string decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: '',
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 0, 'should serialize token decimals correctly')
+  t.end()
+})
+
+test('token with BN.js decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: new BN(10),
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 10, 'should serialize token decimals correctly')
+  t.end()
+})
+
+test('token with array decimals', async function (t) {
+  t.plan(1)
+  const { addresses, token: contract } = await setupSimpleTokenEnvironment()
+  const token = new Token({
+    decimals: [10],
+    contract,
+    owner: addresses[0],
+  })
+
+  const serializedToken = token.serialize()
+  t.deepEqual(serializedToken.decimals, 10, 'should serialize token decimals correctly')
+  t.end()
+})
