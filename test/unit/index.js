ba4d40e6613417507a1f99f2ac0402c195780b42
Mark Stacey <markjstacey@gmail.com>
2020-03-30 10:20:38 -0300

    Emit events for initial token updates (#34)
    Previously the Token constructor had triggered an asynchronous update
    of the token metadata and balance. Unfortunately this update wouldn't
    trigger either an `update` after completion or an `error` event after
    failure, leaving the user of the tracker unable to handle the error
    and unable to tell when the metadata update had finished.
    
    This update has been moved into the Token Tracker instead, and now
    emits both `update` and `error` events as appropriate.
    
    The change to the `update` event has been tested by making a transfer
    immediately after the tracker is constructed. By the time the tracker is
    initiated, the initial token update had already been fired, so this results in
    two separate updates. Previously this would have been a single update.
    
    The Token `serialize` method return value has changed to return the
    serialized token rather than an array of symbol, balance, and decimal.
    This was done because it was slightly more convenient, and the old
    return value was never used anywhere anyway.

--- test/unit/index.js
+++ test/unit/index.js
@@ -4,12 +4,61 @@ const TokenTracker = require('../../lib/')
 const { setupSimpleTokenEnvironment } = require('../helper')
 
 test('tracker with minimal options', async function (t) {
-  t.plan(1)
+  t.plan(2)
   let tracker
   try {
     const { provider } = await setupSimpleTokenEnvironment()
     tracker = new TokenTracker({ provider })
+
+    const updates = []
+    tracker.on('update', (tokens) => {
+      updates.push(tokens)
+    })
+
+    await new Promise((resolve) => setTimeout(() => resolve(), 200))
+
+    t.equal(updates.length, 0, 'should have zero updates')
     t.deepEqual(tracker.serialize(), [], 'should serialize as an empty array')
+    t.end()
+  } finally {
+    tracker.stop()
+  }
+})
+
+test('tracker with token added after initialization', async function (t) {
+  t.plan(2)
+  let tracker
+  try {
+    const { addresses, provider, tokenAddress } = await setupSimpleTokenEnvironment()
+    tracker = new TokenTracker({
+      pollingInterval: 20,
+      provider,
+      userAddress: addresses[1],
+    })
+
+    const updates = []
+    tracker.on('update', (tokens) => {
+      updates.push(tokens)
+    })
+
+    await new Promise((resolve) => setTimeout(() => resolve(), 200))
+    tracker.add({ address: tokenAddress })
+    await new Promise((resolve) => setTimeout(() => resolve(), 200))
+
+    t.equal(updates.length, 1, 'should have one update')
+    t.deepEqual(
+      updates,
+      [
+        [{
+          address: tokenAddress,
+          symbol: 'TKN',
+          balance: '0',
+          decimals: 0,
+          string: '0',
+        }],
+      ],
+      'should have expected initial state'
+    )
     t.end()
   } finally {
     tracker.stop()
@@ -248,6 +297,79 @@ test('tracker with minimal token and one block update with changes', async funct
       ],
       'should have expected state for both updates'
     )
+    t.end()
+  } finally {
+    tracker.stop()
+  }
+})
+
+test('tracker with minimal token and one immediate block update with changes', async function (t) {
+  t.plan(1)
+  let tracker
+  try {
+    const { addresses, provider, token, tokenAddress } = await setupSimpleTokenEnvironment()
+    tracker = new TokenTracker({
+      pollingInterval: 20,
+      provider,
+      tokens: [
+        {
+          address: tokenAddress,
+        }
+      ],
+      userAddress: addresses[1],
+    })
+
+    const updates = []
+    tracker.on('update', (tokens) => {
+      updates.push(tokens)
+    })
+    await token.transfer(addresses[1], '110')
+    await new Promise((resolve) => setTimeout(() => resolve(), 200))
+
+    t.deepEqual(
+      updates,
+      [
+        [{
+          address: tokenAddress,
+          symbol: 'TKN',
+          balance: '0',
+          decimals: 0,
+          string: '0',
+        }],
+        [{
+          address: tokenAddress,
+          symbol: 'TKN',
+          balance: '110',
+          decimals: 0,
+          string: '110',
+        }],
+      ],
+      'should have expected state for both updates'
+    )
+    t.end()
+  } finally {
+    tracker.stop()
+  }
+})
+
+test('tracker with broken provider', async function (t) {
+  t.plan(1)
+  let tracker
+  try {
+    tracker = new TokenTracker({
+      provider: {
+        sendAsync: () => {
+          throw new Error('Fake provider error')
+        }
+      },
+      tokens: [{
+        address: '0xf83c3761a5c0042ab9a694d29520aa9cd6788987',
+      }],
+    })
+    tracker.on('error', () => {
+      t.pass('should emit error event')
+    })
+    await new Promise((resolve) => setTimeout(() => resolve(), 200))
     t.end()
   } finally {
     tracker.stop()
